243d4e1ca01f33bccaad6df0157d572cce1b8ce7
Franklin Hu <franklin.who@gmail.com>
2011-09-16 01:23:01 -0700

    Add Assignment 2, Part d
    Includes script to generate filtered data for each station,
    data file for each of top ten stations, model for each station,
    and script run svm_learn over all stations

---
+++ a2/learn_all.sh
@@ -0,0 +1,11 @@
+svm_learn station_12.txt svm_model_12
+svm_learn station_19.txt svm_model_19
+svm_learn station_24.txt svm_model_24
+svm_learn station_38.txt svm_model_38
+svm_learn station_3.txt svm_model_3
+svm_learn station_63.txt svm_model_63
+svm_learn station_65.txt svm_model_65
+svm_learn station_74.txt svm_model_74
+svm_learn station_7.txt svm_model_7
+svm_learn station_80.txt svm_model_80
+
