3a52c985563313735cbba34c042da77b5fb172e5
Franklin Hu <franklin.who@gmail.com>
2011-09-25 22:53:05 -0700

    More base code for decision tree learning

--- a3/decision_tree.py
+++ a3/decision_tree.py
@@ -1,3 +1,20 @@
+# Franklin Hu, Sunil Pedapudi
+# CS 194-10 Machine Learning
+# Fall 2011
+
+# Implementation of internal nodes of a decision tree
 class decision_tree:
-  def __init__(self):
+  def __init__(self, attribute):
+    self.attribute = attribute
+    self.value_to_tree = {}
+
+  def predict(self, example):
     pass
+
+# Implementation of leaf nodes of a decision tree
+class decision_tree_leaf(decision_tree):
+  def __init__(self, value):
+    self.value = value
+
+  def predict(self, example):
+    return self.value
--- a3/decision_tree_learn.py
+++ a3/decision_tree_learn.py
@@ -1,8 +1,14 @@
+# Franklin Hu, Sunil Pedapudi
+# CS 194-10 Machine Learning
+# Fall 2011
+from collections import defaultdict
 from decision_tree import decision_tree
+
+GOAL = 'goal'
 
 def decision_tree_learn(examples, attributes, parent_examples):
   # If no examples, use pluraity of parent
-  if len(examples) == 0:
+  if not examples:
     return plurality_value(parent_examples)
 
   # If all are of the same class, use that class
@@ -11,19 +17,36 @@ def decision_tree_learn(examples, attributes, parent_examples):
     return common_class
 
   # If there are no more attributes left, use plurality
-  if len(attributes) == 0:
+  if not attributes:
     return plurality_value(examples)
 
   A = importance(attributes, examples)
   tree = decision_tree(A)
 
-  raise Exception("UNIMPLEMENTED")
+  raise Exception("NOT FINISHED")
 
 def plurality_value(examples):
-  pass
+  if not examples:
+    raise Exception('plurality_value called on empty examples list')
+
+  counts = defaultdict(int)
+  for e in examples:
+    counts[GOAL] += 1
+
+  attr_vals = counts.keys()
+  attr_vals.sort(cmp=lambda x,y: cmp(counts[x], counts[y]), reverse=True)
+  return attr_vals[0]
 
 def same_class(attribute, examples):
-  pass
+  attr_val = None
+  for e in examples:
+    if attr_val is None:
+      attr_val = e[attribute]
+    elif not e[attribute] == attr_val:
+      return False
+
+  # All the same so create a leaf node with the common goal
+  return decision_tree_leaf(attr_val)
 
 def importance(attributes, examples):
   pass
