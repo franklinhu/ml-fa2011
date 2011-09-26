eb643a400e1a2dfcebbe738d8dd46625c89af122
Franklin Hu <franklin.who@gmail.com>
2011-09-26 13:20:01 -0700

    Add info gain code

--- a3/decision_tree_learn.py
+++ a3/decision_tree_learn.py
@@ -3,9 +3,11 @@
 # Fall 2011
 import csv
 from collections import defaultdict
-from decision_tree import decision_tree
+import math
 import pickle
 import sys
+
+from decision_tree import decision_tree
 
 GOAL = 'goal'
 STATIONS = []
@@ -24,7 +26,7 @@ def decision_tree_learn(examples, attributes, parent_examples):
   if not attributes:
     return plurality_value(examples)
 
-  A = importance(attributes, examples)
+  A = get_best_attribute(attributes, examples)
   tree = decision_tree(A)
 
   for exs in group_by_attribute(A, examples):
@@ -33,16 +35,32 @@ def decision_tree_learn(examples, attributes, parent_examples):
   return tree
 
 def plurality_value(examples):
+  counts = get_goal_counts(examples)
+  best_attr, best_val = get_plurality(counts)
+  return decision_tree_leaf(best_attr)
+
+def get_plurality_fraction(examples):
+  counts = get_goal_counts(examples)
+  best_attr, best_val = get_plurality(counts)
+  return float(best_val) / len(examples)
+
+def get_goal_counts(examples):
   if not examples:
-    raise Exception('plurality_value called on empty examples list')
+    raise Exception('get_goal_counts called on empty examples list')
 
   counts = defaultdict(int)
   for e in examples:
-    counts[GOAL] += 1
+    counts[e[GOAL]] += 1
+  return counts
 
-  attr_vals = counts.keys()
-  attr_vals.sort(cmp=lambda x,y: cmp(counts[x], counts[y]), reverse=True)
-  return attr_vals[0]
+def get_plurality(counts):
+  best_attr = None
+  best_val = 0
+  for k in counts:
+    if counts[k] > best_val:
+      best_attr = k
+      best_val = counts[k]
+  return best_attr, best_val
 
 def same_class(attribute, examples):
   attr_val = None
@@ -55,8 +73,43 @@ def same_class(attribute, examples):
   # All the same so create a leaf node with the common goal
   return decision_tree_leaf(attr_val)
 
-def importance(attributes, examples):
-  pass
+def get_best_attribute(attributes, examples):
+  best_attr = None
+  best_gain = 0
+  for a in attributes:
+    gain = get_info_gain(a, examples)
+    if gain > best_gain:
+      best_attr = a
+      best_gain = gain
+  return best_attr
+
+def get_info_gain(attribute, examples):
+  total = len(examples)
+  bucketed = bucket_examples_by_attribute(attribute, examples)
+
+  remainder = get_remainder_over_buckets(bucketed, total)
+  
+  base_info = get_plurality_fraction(examples)
+  return base_info - remainder
+
+def bucket_examples_by_attribute(attribute, examples):
+  bucketed = defaultdict(list)
+  for e in examples:
+    bucketed[e[attribute]].append(e)
+  return bucketed
+
+def get_remainder_over_buckets(bucketed, total_count):
+  remainder = 0
+  keys = bucketed.keys()
+  for k in keys:
+    elems = bucketed[k]
+    weight = float(len(elems)) / total_count
+    q = get_plurality_fraction(elems)
+    remainder += weight * bool_entropy(q)
+  return remainder
+
+def bool_entropy(q):
+  return -(q * math.log(q, 2) + (1 - q) * math.log(1 - q, 2))
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
