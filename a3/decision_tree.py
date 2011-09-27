9d41bf881ba4da60131029fef68bf63ea197bb66
Franklin Hu <franklin.who@gmail.com>
2011-09-27 01:50:17 -0700

    Add support for continuous attributes
    Add code for supporting continuous attributes along with discrete
    attributes. Discrete attributes, for now, are hard coded at top. Attributes
    are referred to by their list index rather than by name (there are name
    collisions in the schema in the first line of trainingData.csv).

--- a3/decision_tree.py
+++ a3/decision_tree.py
@@ -15,6 +15,26 @@ class decision_tree:
     value = example[self.attribute]
     return self.value_to_tree[value].classify(example)
 
+class continuous_decision_tree:
+  def __init__(self, attribute, split_point):
+    self.attribute = attribute
+    self.split_point = split_point
+
+  def get_split_point(self):
+    return self.split_point
+
+  def add_less_than_subtree(self, subtree):
+    self.lt_subtree = subtree
+
+  def add_greater_than_subtree(self, subtree):
+    self.gt_subtree = subtree
+
+  def classify(self, example):
+    value = example[self.attribute]
+    if value < split_point:
+      return self.lt_subtree.classify(example)
+    return self.gt_subtree.classify(example)
+
 # Implementation of leaf nodes of a decision tree
 class decision_tree_leaf(decision_tree):
   def __init__(self, value):
--- a3/decision_tree_learn.py
+++ a3/decision_tree_learn.py
@@ -9,28 +9,33 @@ import sys
 
 from decision_tree import *
 
-GOAL = 'goal'
-STATIONS = []
+DISCRETE_ATTRIBUTES = [1, 9, 10, 13, 14, 27, 27, 47, 54]
+GOAL = 'phase'
+GOAL_INDEX = 28
+STATIONS = [7, 24, 3, 80, 19, 38, 63, 12, 74, 65]
+STATION_INDEX = 12
 
 class Example:
-  def __init__(self, datum, schema):
-    if not len(datum) == len(schema):
-      raise Exception('Schema does not match example')
-
-    self.data = {}
-    for i in xrange(len(schema)):
-      self.data[schema[i]] = datum[i]
+  def __init__(self, datum):
+    self.data = datum
   
-  def get(self, attribute):
-    return self.data[attribute]
+  def get(self, attribute_index):
+    val = self.data[attribute_index]
+    if attribute_index in DISCRETE_ATTRIBUTES:
+      return val
+    return float(val)
+
+  def get_goal(self):
+    return self.data[GOAL_INDEX]
 
 def decision_tree_learn(examples, attributes, parent_examples):
-  # If no examples, use pluraity of parent
+  print "+ Decision Tree Learn"
+  # If no examples, use plurality of parent
   if not examples:
     return plurality_value(parent_examples)
 
   # If all are of the same class, use that class
-  common_class = same_class(attribute, examples):
+  common_class = same_class(examples)
   if common_class:
     return common_class
 
@@ -38,12 +43,24 @@ def decision_tree_learn(examples, attributes, parent_examples):
   if not attributes:
     return plurality_value(examples)
 
-  A = get_best_attribute(attributes, examples)
-  tree = decision_tree(A)
+  print "++ Computing best attribute"
+  A, tree = get_best_attribute(attributes, examples)
 
-  for exs in group_by_attribute(A, examples):
-    subtree = decision_tree_learn(exs, list(attribtues).remove(A), examples)
-    tree.add_subtree(A, subtree)
+  print "++ Splitting on attribute: %s" % A
+  if A in DISCRETE_ATTRIBUTES:
+    for attr_val, exs in group_by_attribute(A, examples):
+      subtree = decision_tree_learn(exs, list(attributes).remove(A), 
+                                    examples)
+      tree.add_subtree(attr_val, subtree)
+    return tree
+  
+  # Since all the attributes are continuous, do binary split
+  lt_exs, gt_exs = split_by_attribute(A, tree.get_split_point(), examples)
+  lt_subtree = decision_tree_learn(lt_exs, attributes, examples)
+  gt_subtree = decision_tree_learn(gt_exs, attributes, examples)
+
+  tree.add_less_than_subtree(lt_subtree)
+  tree.add_greater_than_subtree(gt_subtree)
   return tree
 
 def plurality_value(examples):
@@ -62,7 +79,7 @@ def get_goal_counts(examples):
 
   counts = defaultdict(int)
   for e in examples:
-    counts[e.get(GOAL)] += 1
+    counts[e.get_goal()] += 1
   return counts
 
 def get_plurality(counts):
@@ -74,12 +91,12 @@ def get_plurality(counts):
       best_val = counts[k]
   return best_attr, best_val
 
-def same_class(attribute, examples):
+def same_class(examples):
   attr_val = None
   for e in examples:
     if attr_val is None:
-      attr_val = e.get(attribute)
-    elif not e.get(attribute) == attr_val:
+      attr_val = e.get_goal()
+    elif not e.get_goal() == attr_val:
       return False
 
   # All the same so create a leaf node with the common goal
@@ -88,14 +105,56 @@ def same_class(attribute, examples):
 def get_best_attribute(attributes, examples):
   best_attr = None
   best_gain = 0
+  best_split_point = None
   for a in attributes:
-    gain = get_info_gain(a, examples)
+    if a in DISCRETE_ATTRIBUTES:
+      gain = discrete_info_gain(a, examples)
+      split_point = None
+    else:
+      gain, split_point = continuous_info_gain(a, examples)
+    
+    print "+++ Tried attribute: %s %f" % (a, gain)
     if gain > best_gain:
-      best_attr = a
       best_gain = gain
-  return best_attr
+      best_attr = a
+      best_split_point = split_point
 
-def get_info_gain(attribute, examples):
+  if best_attr in DISCRETE_ATTRIBUTES:
+    return best_attr, decision_tree(best_attr)
+  return best_attr, continuous_decision_tree(best_attr, best_split_point)
+
+def continuous_info_gain(attribute, examples):
+  sortable = list(examples)
+  sortable.sort(cmp=lambda x,y: cmp(x.get(attribute), y.get(attribute)))
+
+  index = 0
+  total = len(sortable)
+  best_gain = 0
+  best_index = index
+  while index < len(sortable):
+    # Skip over elements that have the same class under this attribute
+    current_goal = sortable[index].get_goal()
+    while (index < len(sortable)) and \
+          (current_goal == sortable[index].get_goal()):
+      index += 1
+    if index >= len(sortable):
+      continue
+
+    left = sortable[:index]
+    right = sortable[index:]
+    l_entropy = bool_entropy(get_plurality_fraction(left))
+    r_entropy = bool_entropy(get_plurality_fraction(right))
+    gain = (float(len(left)) / total) * l_entropy + \
+           (float(len(right)) / total) * r_entropy
+    if gain > best_gain:
+      best_gain = gain
+      best_index = index
+  split_point = float(sum([e.get(attribute) for e in 
+                          sortable[best_index-1:best_index+1]])) / 2
+
+  return best_gain, split_point
+
+def discrete_info_gain(attribute, examples):
   total = len(examples)
   bucketed = bucket_examples_by_attribute(attribute, examples)
 
@@ -120,7 +179,19 @@ def get_remainder_over_buckets(bucketed, total_count):
     remainder += weight * bool_entropy(q)
   return remainder
 
+def split_by_attribute(attribute, split_point, examples):
+  lt_exs = []
+  gt_exs = []
+  for e in examples:
+    if e.get(attribute) < split_point:
+      lt_exs.append(e)
+    else:
+      gt_exs.append(e)
+  return lt_exs, gt_exs
+
 def bool_entropy(q):
+  if (q == 0) or (q == 1):
+    return -math.log(1, 2)
   return -(q * math.log(q, 2) + (1 - q) * math.log(1 - q, 2))
 
 if __name__ == "__main__":
@@ -131,13 +202,22 @@ if __name__ == "__main__":
   input_file = sys.argv[1]
   data = csv.reader(open(input_file, 'r'))
   examples = defaultdict(list)
+
+  # The first row of the CSV file is the schema (column names)
+  schema = data.next()
+
   for datum in data:
-    examples[datum[STATION_INDEX]].append(Example(datum))
+    if not datum:
+      continue
+    examples[int(datum[STATION_INDEX])].append(Example(datum))
+
+  attributes = range(len(schema))
+  attributes.pop(GOAL_INDEX)
   
   for station in STATIONS:
-    e = examples[station]
-    dt = decision_tree_learn(examples, attributes, examples)
-    dt.prune()
+    exs = examples[station]
+    dt = decision_tree_learn(exs, attributes, exs)
+    # dt.prune()
     outfile = "tree_%s" % station
     handle = open(outfile, 'w')
     handle.write(pickle.dump(dt))
