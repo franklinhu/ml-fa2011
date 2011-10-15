5568e516484104c0fd3f06c75c4ba145beed9585
Franklin Hu <franklin.who@gmail.com>
2011-10-15 13:33:48 -0700

    Add classification stubs (4d)

---
+++ a4/a4_classify.py
@@ -0,0 +1,10 @@
+# Franklin Hu, Sunil Pedapudi
+# CS 194-10 Machine Learning
+# Fall 2011
+# Assignment 4
+
+def NBclassify_Boolean(model, example, cost_ratio):
+  pass
+
+def NBclassify_NTF(mode, example, cost_ratio):
+  pass
