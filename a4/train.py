31fb8eec2c62c5c3393e8827330552cebef9978f
Sunil Pedapudi <skpedapudi@gmail.com>
2011-10-20 18:13:28 -0700

    Add: pickler for freq features

--- a4/train.py
+++ a4/train.py
@@ -2,6 +2,7 @@
 # CS 194-10 Machine Learning
 # Fall 2011
 # Assignment 4
+from collections import defaultdict
 import pickle
 import sys
 
@@ -24,6 +25,25 @@ def dump_obj(filename, obj):
     pickle.dump(obj, handle)
     handle.close()
 
+def _write_features(features_file, spam_dir, ham_dir):
+    freq = defaultdict(int)
+    for email_file in NBmodel.get_files(spam_dir):
+        email = open(email_file, 'r')
+        for line in email:
+            tok = line.strip(" ").split(" ")
+            for t in tok:
+                freq[('SPAM',t)] += 1
+        email.close()
+    for email_file in NBmodel.get_files(ham_dir):
+        email = open(email_file, 'r')
+        for line in email:
+            tok = line.strip(" ").split(" ")
+            for t in tok:
+                freq[('HAM',t)] += 1
+        email.close()
+
+    dump_obj(features_file, freq)
+
 if __name__ == "__main__":
     if len(sys.argv) < NUM_ARGS + 1:
         print "Usage: train.py spamdir hamdir bool_features ntf_features"
@@ -33,20 +53,22 @@ if __name__ == "__main__":
     bool_features_file = sys.argv[3]
     ntf_features_file = sys.argv[4]
 
-    bool_features = pickle.load(open(bool_features_file, 'rb'))
-    ntf_features = pickle.load(open(ntf_features_file, 'rb'))
+    _write_features(bool_features_file, spamdir, hamdir)
 
-    bool_model = NBmodel.NB_Boolean()
-    ntf_model = NBmodel.NB_NTF()
+    # bool_features = pickle.load(open(bool_features_file, 'rb'))
+    # ntf_features = pickle.load(open(ntf_features_file, 'rb'))
 
-    train_dir(spamdir, NBmodel.SPAM, bool_model, bool_features, ntf_model,
-              ntf_features)
-    train_dir(hamdir, NBmodel.HAM, bool_model, bool_features, ntf_model,
-              ntf_features)
+    # bool_model = NBmodel.NB_Boolean()
+    # ntf_model = NBmodel.NB_NTF()
 
-    bool_features.finalize_training()
-    ntf_features.finalize_training()
+    # train_dir(spamdir, NBmodel.SPAM, bool_model, bool_features, ntf_model,
+    #           ntf_features)
+    # train_dir(hamdir, NBmodel.HAM, bool_model, bool_features, ntf_model,
+    #           ntf_features)
 
-    dump_obj(BOOL_MODEL_FILE, bool_model)
-    dump_obj(NTF_MODEL_FILE, ntf_model)
+    # bool_features.finalize_training()
+    # ntf_features.finalize_training()
+
+    # dump_obj(BOOL_MODEL_FILE, bool_model)
+    # dump_obj(NTF_MODEL_FILE, ntf_model)
 
