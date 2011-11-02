ba61339a9bdcb5c5bd4437d4743dc4f49fb89c0f
Franklin Hu <franklin.who@gmail.com>
2011-11-01 21:50:15 -0700

    Fix start/stop indices

--- a6/cross_validation.py
+++ a6/cross_validation.py
@@ -1,15 +1,18 @@
 # Franklin Hu, Sunil Pedapudi
 # CS 194-10 Machine Learning
 # Fall 2011
+import math
 import random
 
 class CrossValidation:
     
-    def __init__(self, num_folds, data):
+    def __init__(self, num_folds, data, shuffle=False):
         self.num_folds = num_folds
         self.data = list(data)
-        self.items_per_fold = len(data) / num_folds
-        random.shuffle(self.data)
+        self.items_per_fold = int(math.ceil(float(len(data)) / num_folds))
+
+        if shuffle:
+            random.shuffle(self.data)
 
     def training_examples(self, fold_num):
         start,stop = self._training_index_range(fold_num)
@@ -24,6 +27,8 @@ class CrossValidation:
             yield self.data[i]
 
     def _training_index_range(self, fold_num):
+        if fold_num >= self.num_folds:
+            raise Exception("Invalid fold number: %d" % fold_num)
         start = fold_num * self.items_per_fold
         stop = min(len(self.data), start + self.items_per_fold)
         return start, stop
